5a654559a2bb4818b0772f2ee4c7ce1f299ea079
everest275 <eaguilarc112@uacit.ed.cr>
2024-01-11 19:01:43 -0600

    init

--- Services/service.js
+++ Services/service.js
@@ -1,9 +1,6 @@
 import mongose from 'mongoose';
 (async()=>{
-    await mongose.connect('mongodb+srv://everest012016:XW9cbh0I84zJvbsh@myapp.pbkqwos.mongodb.net/?retryWrites=true&w=majority',{
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
+    await mongose.connect('mongodb+srv://everest012016:XW9cbh0I84zJvbsh@myapp.pbkqwos.mongodb.net/?retryWrites=true&w=majority')
     .then(()=>console.log('Base de datos conectada'))
     .catch(err=>console.log('Error db conect::',err))
 })()
