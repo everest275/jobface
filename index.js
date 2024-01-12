12623f9cdfa3bc7e93627d81a122ea87b8b31515
everest275 <eaguilarc112@uacit.ed.cr>
2024-01-11 18:38:06 -0600

    init

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import app from './app.js'
 import './Services/service.js'
-
+const port=8085
 app.listen(process.env.PORT || 8085,()=>{
-    console.log('Esta corriento con server http://localhost:'+process.env.PORT);
+    console.log('Esta corriento con server http://localhost:'+port);
 });
